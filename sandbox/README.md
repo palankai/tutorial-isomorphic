ed25eb6a13ccaaf9ab2f5eb9d2ced9ac9b8494e0
Csaba Palankai <csaba.palankai@gmail.com>
2017-08-30 11:38:54 +0100

    Cleanup unneeded code

--- sandbox/README.md
+++
@@ -1,1 +0,0 @@
-# This folder contains internal work only
--- sandboxes/sandbox-5-redux/frontend/src/components/Content/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Content = () => (
-  <p>This is my first ReactJS page</p>
-);
-
-export default Content;

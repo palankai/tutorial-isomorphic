749a8c2afaad7c94dea8cbf91f27cc811b5cee54
Csaba Palankai <csaba.palankai@gmail.com>
2017-09-01 14:52:50 +0100

    Implement components (WIP)

--- final/frontend/client/containers/Submit/index.jsx
+++ final/frontend/client/containers/Submit/index.jsx
@@ -1,36 +1,9 @@
 import React from 'react';
+import Navigation from 'components/Navigation';
 
 const Submit = () => (
   <div>
-    <nav className="navbar navbar-inverse navbar-static-top">
-      <div className="container">
-        <div className="navbar-header">
-          <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span className="sr-only">Toggle navigation</span>
-            <span className="icon-bar"></span>
-            <span className="icon-bar"></span>
-            <span className="icon-bar"></span>
-          </button>
-          <a className="navbar-brand" href="/">ADR database</a>
-        </div>
-        <div id="navbar" className="navbar-collapse collapse">
-          <ul className="nav navbar-nav">
-            <li><a href="/">Home</a></li>
-            <li className="active"><a href="#submit">Submit</a></li>
-          </ul>
-          <form className="navbar-form navbar-right" action="/">
-            <div className="input-group">
-            <input type="text" name="byText" className="form-control" placeholder="Search..." />
-            <span className="input-group-btn">
-              <button className="btn btn-default" type="button">
-                <span className="glyphicon glyphicon-search" aria-hidden="true"></span>
-              </button>
-            </span>
-            </div>
-          </form>
-        </div>
-      </div>
-    </nav>
+    <Navigation />
 
     <div className="container">
       <div className="row">
